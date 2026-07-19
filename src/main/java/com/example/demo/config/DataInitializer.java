e750401278a6007f1df692748d71c8cafa191bc6
IsabelaMunzlinger <isabelamunzlinger@gmail.com>
2026-07-19 11:37:57 -0300

    fix: remove usuário admin padrão porque vai ser usado o seeder

--- src/main/java/com/example/demo/config/DataInitializer.java
+++ src/main/java/com/example/demo/config/DataInitializer.java
@@ -18,18 +18,8 @@ public class DataInitializer implements CommandLineRunner {
         this.passwordEncoder = passwordEncoder;
     }
 
-    //Se o banco estiver vazio, traz um usuário admin padrão para acessar
     @Override
     public void run(String... args) throws Exception {
-        if (usuarioRepository.count() == 0) {
-            Usuario admin = new Usuario();
-            admin.setNome("Usuário Admin");
-            admin.setEmail("admin.gmail.com");
-            admin.setSenha(passwordEncoder.encode("123456"));
-            admin.setPerfil(Perfil.ADMIN);
-            admin.setAtivo(true);
 
-            usuarioRepository.save(admin);
-        }
     }
 }
