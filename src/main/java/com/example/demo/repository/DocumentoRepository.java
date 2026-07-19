f3f861b1094c0217c83596a73f4179431f593861
IsabelaMunzlinger <isabelamunzlinger@gmail.com>
2026-07-19 00:16:46 -0300

    feat: adiciona repository de documento

---
+++ src/main/java/com/example/demo/repository/DocumentoRepository.java
@@ -0,0 +1,5 @@
+package com.example.demo.repository;
+import com.example.demo.model.Documento;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DocumentoRepository extends JpaRepository<Documento, Long> {}
